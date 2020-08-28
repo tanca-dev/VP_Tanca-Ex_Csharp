64ca9826c7690712ab59246469f89cb2c5a62c6d
desenv-tanca <gildo@tanca.com.br>
2020-08-28 11:55:35 -0300

    Atualização

---
+++ wfValidou/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace wfValidou
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnValida = new System.Windows.Forms.Button();
+            this.LTerminal = new System.Windows.Forms.Label();
+            this.LbMensagens = new System.Windows.Forms.ListBox();
+            this.LMensagens = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.LbTerminal = new System.Windows.Forms.ListBox();
+            this.BtEnviar = new System.Windows.Forms.Button();
+            this.TbTempo = new System.Windows.Forms.TextBox();
+            this.TbLinha2 = new System.Windows.Forms.TextBox();
+            this.TbLinha1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.TmTerminal = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnValida
+            // 
+            this.BtnValida.BackColor = System.Drawing.Color.GhostWhite;
+            this.BtnValida.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnValida.ForeColor = System.Drawing.Color.Blue;
+            this.BtnValida.Location = new System.Drawing.Point(12, 513);
+            this.BtnValida.Name = "BtnValida";
+            this.BtnValida.Size = new System.Drawing.Size(136, 39);
+            this.BtnValida.TabIndex = 0;
+            this.BtnValida.Text = "Valide a DLL";
+            this.BtnValida.UseVisualStyleBackColor = false;
+            this.BtnValida.Click += new System.EventHandler(this.BtnValida_Click);
+            // 
+            // LTerminal
+            // 
+            this.LTerminal.AutoSize = true;
+            this.LTerminal.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.LTerminal.Location = new System.Drawing.Point(11, 26);
+            this.LTerminal.Name = "LTerminal";
+            this.LTerminal.Size = new System.Drawing.Size(62, 13);
+            this.LTerminal.TabIndex = 2;
+            this.LTerminal.Text = "TERMINAL";
+            // 
+            // LbMensagens
+            // 
+            this.LbMensagens.FormattingEnabled = true;
+            this.LbMensagens.Location = new System.Drawing.Point(415, 43);
+            this.LbMensagens.Name = "LbMensagens";
+            this.LbMensagens.Size = new System.Drawing.Size(278, 238);
+            this.LbMensagens.TabIndex = 1;
+            // 
+            // LMensagens
+            // 
+            this.LMensagens.AutoSize = true;
+            this.LMensagens.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.LMensagens.Location = new System.Drawing.Point(486, 15);
+            this.LMensagens.Name = "LMensagens";
+            this.LMensagens.Size = new System.Drawing.Size(133, 13);
+            this.LMensagens.TabIndex = 2;
+            this.LMensagens.Text = "MENSAGENS INTERNAS";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LbTerminal);
+            this.groupBox1.Controls.Add(this.BtEnviar);
+            this.groupBox1.Controls.Add(this.TbTempo);
+            this.groupBox1.Controls.Add(this.TbLinha2);
+            this.groupBox1.Controls.Add(this.TbLinha1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.LTerminal);
+            this.groupBox1.Location = new System.Drawing.Point(424, 296);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 256);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // LbTerminal
+            // 
+            this.LbTerminal.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.LbTerminal.Enabled = false;
+            this.LbTerminal.FormattingEnabled = true;
+            this.LbTerminal.Location = new System.Drawing.Point(14, 45);
+            this.LbTerminal.Name = "LbTerminal";
+            this.LbTerminal.Size = new System.Drawing.Size(228, 17);
+            this.LbTerminal.TabIndex = 5;
+            // 
+            // BtEnviar
+            // 
+            this.BtEnviar.Location = new System.Drawing.Point(81, 198);
+            this.BtEnviar.Name = "BtEnviar";
+            this.BtEnviar.Size = new System.Drawing.Size(92, 39);
+            this.BtEnviar.TabIndex = 4;
+            this.BtEnviar.Text = "ENVIAR";
+            this.BtEnviar.UseVisualStyleBackColor = true;
+            this.BtEnviar.Click += new System.EventHandler(this.BtEnviar_Click);
+            // 
+            // TbTempo
+            // 
+            this.TbTempo.Location = new System.Drawing.Point(14, 159);
+            this.TbTempo.MaxLength = 255;
+            this.TbTempo.Name = "TbTempo";
+            this.TbTempo.Size = new System.Drawing.Size(228, 20);
+            this.TbTempo.TabIndex = 3;
+            this.TbTempo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TbTempo_KeyPress);
+            // 
+            // TbLinha2
+            // 
+            this.TbLinha2.Location = new System.Drawing.Point(14, 119);
+            this.TbLinha2.MaxLength = 50;
+            this.TbLinha2.Name = "TbLinha2";
+            this.TbLinha2.Size = new System.Drawing.Size(228, 20);
+            this.TbLinha2.TabIndex = 3;
+            // 
+            // TbLinha1
+            // 
+            this.TbLinha1.Location = new System.Drawing.Point(14, 82);
+            this.TbLinha1.MaxLength = 50;
+            this.TbLinha1.Name = "TbLinha1";
+            this.TbLinha1.Size = new System.Drawing.Size(228, 20);
+            this.TbLinha1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(11, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "TEMPO DE APRESENTAÇÃO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DIÁLOGO COM O TERMINAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(11, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "LINHA 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(11, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "LINHA 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = global::wfValidou.Properties.Resources.vp_640wsitepng5ea1a4d1d5aca;
+            this.ClientSize = new System.Drawing.Size(705, 564);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.LMensagens);
+            this.Controls.Add(this.LbMensagens);
+            this.Controls.Add(this.BtnValida);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Validou! - Exemplo C#";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnValida;
+        private System.Windows.Forms.Label LTerminal;
+        private System.Windows.Forms.ListBox LbMensagens;
+        private System.Windows.Forms.Label LMensagens;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BtEnviar;
+        private System.Windows.Forms.TextBox TbTempo;
+        private System.Windows.Forms.TextBox TbLinha2;
+        private System.Windows.Forms.TextBox TbLinha1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Timer TmTerminal;
+        private System.Windows.Forms.ListBox LbTerminal;
+    }
+}
+
---
+++ wfValidou/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace wfValidou
+{
+    public partial class Form1 : Form
+    {
+        //Função que Inicializa Serviço
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern void vInitialize();
+
+        //Função que Inicializa Servidor
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern int tc_startserver();
+
+        //Função que Identifica Terminais Conectados ao Servidor
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static unsafe extern bool bConnected(out uint ID_IP, out ushort Porta);
+
+        //Função que Identifica Terminais Desconectados do Servidor
+        //[DllImport("VP_v3_x64.dll")] //----> X64
+        [DllImport("VP_v3.dll")] //----> X86
+        public static extern unsafe bool bDisconnected(out uint ID_IP, out ushort ID_Socket);
+
+        //Função que Desconecta dado Terminal
+        //[DllImport("VP_v3_x64.dll")] //----> X64
+        [DllImport("VP_v3.dll")] //----> X86
+        public static extern unsafe bool bCloseTerminal(uint ID_IP, ushort ID_Socket);
+
+        //Formatação de Valores
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern unsafe IntPtr Inet_NtoA(uint ID_IP);
+
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern unsafe uint Inet_Addr(IntPtr sIP);
+
+        //Função que busca a versão da DLL
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        static extern IntPtr dll_version();
+
+        //Função para enviar mensagem para as linhas 1 e 2 do terminal
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern unsafe bool bSendDisplayMsg(uint ID_IP, IntPtr Linha1, IntPtr Linha2, int Tempo);
+
+        //Função usada periodicamente para identificar se houve solicitação de pesquisa de código de barras pendente. 
+        //Caso haja, retornará o código de barras (ponteiro string), o endereço IP e a porta do terminal solicitante;
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern unsafe IntPtr bReceiveBarcode(out uint ID_IP, out ushort ID_Socket, out int Nbr);
+
+        //Função usada para Informar a aplicação que o terminal solicitante recebeu a mensagem;
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern unsafe bool bSendProdNotFound(uint ID_IP);
+
+        //Envia para a primeira e segunda linha do display do terminal solicitante o nome e o preço do produto pesquisado;
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern unsafe bool bSendProdPrice(UInt32 IP, IntPtr Prod, IntPtr Valor);
+
+        //Função que Finaliza Serviço
+        //[DllImport("VP_v3_x64.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X64
+        [DllImport("VP_v3.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Ansi)] //----> X86
+        public static extern bool bTerminate();
+
+        //Neste Exemplo você assistirá o uso de uma variavel fixa para representar o valor do terminal identificado como conectado
+        //Mas, na prática, você deverá desenvolver um forma de controle para essa informação, para poder utilizar e fazer referência 
+        //ao terminais conectado, você pode utilizar meios como tabela, grid, etc
+        uint IpTerminal = 0;
+
+        public Form1()
+        {
+           InitializeComponent();
+            //Solicitando o serviço
+            Servico();
+        }
+        private void Servico()
+        {
+            vInitialize();
+            LbMensagens.Items.Add("Serviço Inicializado!");
+
+            if (tc_startserver() == 1)
+            {
+                LbMensagens.Items.Add("Servidor a todo vapor!");
+
+                Timer.Interval = 10000;
+                Timer.Tick += new EventHandler(Timer_Tick);
+                Timer.Enabled = true;
+            }
+            else
+            {
+                LbMensagens.Items.Add("Servidor Não inicializado!");
+            }
+            
+        }
+
+        private void BtnValida_Click(object sender, EventArgs e)
+        {
+            IntPtr dados = dll_version();
+            
+            //A maneira como se pretende apresentar essa informação pode ser tratada, estéticamente caso veja necessidade
+            LbMensagens.Items.Add("Versão da DLL é  " + dados);
+                        
+        }
+        private void Timer_Tick(object Sender, EventArgs e)
+        {
+
+            LbMensagens.Items.Add(DateTime.Now.ToString());
+            if (bConnected(out uint Term, out ushort Porta))
+            {
+                LbMensagens.Items.Add("Novo Terminal " + Term + " conectado na porta " + Porta);
+
+                IpTerminal = Term;
+            }
+            else
+            {
+                //Você não precisa desenvolver esse tipo de informação, essa mensagem serve apenas como referência
+                LbMensagens.Items.Add("Não conectou novo Terminal");
+            }
+
+            TmTerminal.Interval = 10000;
+            TmTerminal.Tick += new EventHandler(TmTerminal_Tick);
+            TmTerminal.Enabled = true;
+            Timer.Enabled = false;
+        }
+        private void TmTerminal_Tick(object Sender, EventArgs e)
+        {
+            UInt32 CodBarras = 123;
+            UInt32 Teste = 0;
+
+            //Será necessário que você desenvolva um mecanismo de comunicação entre sua aplicação e sua base de dados
+            //Os valores fixos servem apenas como referencia
+            string ProdDados = "LIMAO";
+            string ValorDados = "2,00";
+
+            IntPtr Cod = Marshal.StringToHGlobalAnsi("0");
+            IntPtr Prod = Marshal.StringToHGlobalAnsi(ProdDados);
+            IntPtr Valor = Marshal.StringToHGlobalAnsi(ValorDados);
+
+            try
+            {
+                Cod = bReceiveBarcode(out uint Term, out ushort Porta, out int QtCod);
+
+                string CodBar = Marshal.PtrToStringAnsi(Cod);
+
+                try
+                {
+                    if (Term > 0)
+                    {
+                        if (CodBarras == Convert.ToInt32(CodBar))
+                        {
+                            //Você não pode montar o retorno de dados conforme a sua estratégia
+                            LbMensagens.Items.Add("O produto " + CodBar + "existe? By: " + Term + " ");
+
+                            _ = bSendProdPrice(Term, Prod, Valor);
+                            LbMensagens.Items.Add("Sim " + Term);
+                        }
+                        else
+                        {
+                            LbMensagens.Items.Add("O produto " + CodBar + "existe? By: " + Term + " ");
+
+                            _ = bSendProdNotFound(Term);
+
+                            LbMensagens.Items.Add("Não existe");
+
+                            /* Outra maneira de fazer
+                            Prod = Marshal.StringToHGlobalAnsi("NAO EXISTENTE");
+                            Valor = Marshal.StringToHGlobalAnsi("0");
+
+                            _ = bSendProdPrice(Term, Prod, Valor);*/
+                        }
+                    }
+                }
+                catch
+                {
+                    if (Term > 0)
+                    {
+
+                        if (CodBarras == Teste)
+                        {
+                            LbMensagens.Items.Add("O produto " + CodBar + "existe? By: " + Term + " ");
+
+                            _ = bSendProdPrice(Term, Prod, Valor);
+                        }
+                        else
+                        {
+                            LbMensagens.Items.Add("O produto " + CodBar + "existe? By: " + Term + " ");
+
+                            _ = bSendProdNotFound(Term);
+
+                            LbMensagens.Items.Add("Não existe");
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                LbMensagens.Items.Add("Aguardando Pesquisa.");
+            }
+            TmTerminal.Enabled = false;
+            Timer.Enabled = true;
+        }
+
+        private void BtEnviar_Click(object sender, EventArgs e)
+        {
+            LbTerminal.Items.Add(IpTerminal);
+
+            if (TbLinha1.Text != "" && TbLinha2.Text != "" && TbTempo.Text != "")
+            {
+                int Tempo = Convert.ToInt32(TbTempo.Text.Trim());
+                string aux = TbLinha1.Text.Trim();
+                string aux1 = TbLinha2.Text.Trim();
+
+                IntPtr Linha1 = Marshal.StringToHGlobalAnsi(aux);
+                IntPtr Linha2 = Marshal.StringToHGlobalAnsi(aux1);
+
+                try
+                {
+                    if (bSendDisplayMsg(IpTerminal, Linha1, Linha2, Tempo) == true)
+                    {
+                        LbMensagens.Items.Add("Mensagem Enviada");
+                    }
+                    else
+                    {
+                        LbMensagens.Items.Add("RETORNO FALSO");
+                    }
+                }
+                catch
+                {
+                    LbMensagens.Items.Add("Dados invalidos");
+                }
+            }
+            else
+            {
+                LbMensagens.Items.Add("Preencha os campos");
+            }
+
+            TbLinha1.Text = "";
+            TbLinha2.Text = "";
+            TbTempo.Text = "";
+            LbTerminal.Items.Clear();
+        }
+
+        private void TbTempo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Se a tecla digitada não for número e nem backspace
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08)
+            {
+                //Atribui True no Handled para cancelar o evento
+                e.Handled = true;
+            }
+        }
+    }
+}
